3aace17c694f5c9031caede9507733cef2803a9d
Alexandre Ferraz - MAC <alexandrehpf@hotmail.com>
2025-05-02 10:54:29 -0300

    Finalizando o crud

---
+++ src/controllers/PessoaControllers.js
@@ -0,0 +1,13 @@
+const Controller = require('./Controller.js')
+const PessoaServices = require('../services/PessoaServices.js');
+
+const pessoasServices = new PessoaServices();
+
+class PessoaController extends Controller {
+  constructor() {
+    super(pessoasServices);
+
+  }
+}
+
+module.exports = PessoaController;  
---
+++ src/services/PessoaServices.js
@@ -0,0 +1,11 @@
+const Services = require('./Services.js');
+
+class PessoaServices extends Services { 
+    constructor() {
+        super('Pessoa');
+    }
+
+}
+
+module.exports = PessoaServices;
+
