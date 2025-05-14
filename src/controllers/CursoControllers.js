c21f2389ab532cb67d742a40aa8066c3d52c4c2f
Alexandre Ferraz - MAC <alexandrehpf@hotmail.com>
2025-05-14 06:15:12 -0300

    Subindo os arquivos para consulta de query string

--- src/controllers/CursoControllers.js
+++ src/controllers/CursoControllers.js
@@ -1,10 +1,10 @@
 const { Op } = require('sequelize');
-const Controller = require('./Controller.js')
+const Controller = require('./Controller.js');
 const CursoServices = require('../services/CursoServices.js');
 
 const cursoServices = new CursoServices();
 
-class CurosController extends Controller {
+class CursoController extends Controller {
   constructor() {
     super(cursoServices);
   }
@@ -12,13 +12,22 @@ class CurosController extends Controller {
   async pegaCursos(req, res) {
     const { data_inicial, data_final } = req.query;
     const where = {};
+
     //se existirem os param, criar uma prop {}
     data_inicial || data_final ? where.data_inicio = {} : null;
     // se existir data inicial, adiciona a prop gte com o valor
     data_inicial ? where.data_inicio[Op.gte] = data_inicial : null;
     // se existir data final, idem
     data_final ? where.data_inicio[Op.lte] = data_final : null;
+
+    try {
+      const listaCursos = await cursoServices.pegaTodosOsRegistros(where);
+      return res.status(200).json(listaCursos);
+    } catch (erro) {
+      return res.status(500).json({ erro: erro.message });
+    }
+
   }
 }
 
-module.exports = CurosController;  
+module.exports = CursoController;
